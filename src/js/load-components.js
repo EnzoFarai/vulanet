c494842cfa3c8929aa2c6afc38e77ba8286d289a
EnzoFarai <enzofarai25@gmail.com>
2026-05-29 20:35:00 +0200

    Update load-components.js

--- src/js/load-components.js
+++ src/js/load-components.js
@@ -1,12 +1,13 @@
+// src/js/load-components.js
 export async function loadHeaderFooter() {
   const headerPlaceholder = document.getElementById('header-placeholder');
   const footerPlaceholder = document.getElementById('footer-placeholder');
   if (headerPlaceholder) {
     const res = await fetch('/src/components/header.html');
-    headerPlaceholder.innerHTML = await res.text();
+    if (res.ok) headerPlaceholder.innerHTML = await res.text();
   }
   if (footerPlaceholder) {
     const res = await fetch('/src/components/footer.html');
-    footerPlaceholder.innerHTML = await res.text();
+    if (res.ok) footerPlaceholder.innerHTML = await res.text();
   }
 }
